228faf7fc4ba426ee243cdc808446d4c29dff8b8
dima <magmus02@gmail.com>
2021-03-03 15:00:35 +0200

    task2 upd

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
+  console.log('task2');
   return (
     <nav className="app-nav">
       <NavLink exact to="/" activeClassName="active">
