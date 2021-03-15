901ef429d4e0f5dc5884ab86f1f97b33b9e19dbb
dima <magmus02@gmail.com>
2021-03-15 10:24:45 +0200

    Squashed commit of the following:
    commit 353bc8cf9f7b02a5ef11de72e0abfe5810a44920
    Author: dima <magmus02@gmail.com>
    Date:   Mon Mar 15 10:21:39 2021 +0200
    
        upd
    
    commit 074c65637a7ea0365736323fe3c35d7d2d2374c8
    Merge: 55afb50 0b388cd
    Author: dima <magmus02@gmail.com>
    Date:   Wed Mar 3 15:54:12 2021 +0200
    
        Merge branch 'release_1' of https://github.com/iDim123/swivl-test into release_1
    
    commit 0b388cd350ede4b16bde297f2d154bd8d772a8fa
    Merge: 8bf3e51 a703468
    Author: dima <magmus02@gmail.com>
    Date:   Wed Mar 3 15:36:58 2021 +0200
    
        Merge branch 'task1' into release_1
    
    commit 8bf3e51f7b337171f707d2094ee49add4a8d95f8
    Merge: ba5f567 5156c50
    Author: dima <magmus02@gmail.com>
    Date:   Wed Mar 3 15:36:52 2021 +0200
    
        Merge branch 'task2' into release_1
    
    commit ba5f567ca733d37b6f44be5494f4d8be6345818d
    Author: dima <magmus02@gmail.com>
    Date:   Wed Mar 3 15:01:27 2021 +0200
    
        task3
    
    commit 5156c50b2da7daa146b3966175f20cc4676127e9
    Author: dima <magmus02@gmail.com>
    Date:   Wed Mar 3 15:00:49 2021 +0200
    
        task2 add 88
    
    commit b65b52ebb6aefd1e02c49aa60f524d02f24feb8a
    Author: dima <magmus02@gmail.com>
    Date:   Wed Mar 3 15:00:35 2021 +0200
    
        task2 upd
    
    commit a7034682e8f0c305739a38c98e96c67667700439
    Author: dima <magmus02@gmail.com>
    Date:   Wed Mar 3 14:59:59 2021 +0200
    
        task1 upd

--- src/containers/Task/Task.js
+++ src/containers/Task/Task.js
@@ -1,33 +1,69 @@
 import './Task.css';
 
-import React from 'react';
+import React, { Fragment, useCallback, useEffect, useMemo, useState } from 'react';
 
 const Task = () => {
+  const [number, setNumber] = useState(1);
+  const [switcher, setSwitcher] = useState(false);
+
+  // const memoA = { color: 'red' };
+  // const memoA = useMemo(() => ({ color: switcher ? 'red' : 'blue' }), [switcher]);
+  const memoA = useCallback(() => ({ color: switcher ? 'red' : 'blue' }), [switcher]);
+
+  useEffect(() => {
+    console.log('swithcsr ch');
+  }, [memoA]);
+
+  function long(number, calc) {
+    console.log('long');
+    let i = 0;
+    while (i < 1000000000) i++;
+    return number * 2 + calc(number);
+  }
+  const calc = (num) => num;
+
+  const memoCalc = useCallback(() => {
+    return calc(number);
+  }, [number]);
+
+  const res = useMemo(() => {
+    return long(number, memoCalc);
+  }, [number, memoCalc]);
+
+  console.log(res, switcher);
   return (
-    <div className="task__container">
-      <h1>Hello, my task is:</h1>
-      <code>
-        github.com has public API to get list of users: "https://api.github.com/users".
-        <br />
-        <br />
-        There are helpfull parameters like "per_page" and "since", check them.
-        <br />
-        You need to create a single page application, which allows to get list of github users.{' '}
-        <br />
-        Each row contains login, profile link (html_url) and avatar preview(100x100). <br />
-        <br />
-        Clicking on row should result in opening new route with bigger avatar version and additional{' '}
-        <br />
-        user info from "https://api.github.com/users/:username", such as name, followers, following,
-        created_at, company, email, location, blog, bio.
-        <br />
-        <br />
-        We expect application will allow to browse at least first 100 users.
-        <br />
-        <br />
-        Using React, ES2015 and Redux is required.
-      </code>
-    </div>
+    <Fragment>
+      <div>
+        <h1>{res}</h1>
+        <h1>{String(switcher)}</h1>
+        <button onClick={() => setNumber((prev) => prev + 1)}>Numbsers</button>
+        <button onClick={() => setSwitcher((prev) => !prev)}>Switcher</button>
+      </div>
+      <div className="task__container">
+        <h1>Hello, my task is:</h1>
+        <code>
+          github.com has public API to get list of users: "https://api.github.com/users".
+          <br />
+          <br />
+          There are helpfull parameters like "per_page" and "since", check them.
+          <br />
+          You need to create a single page application, which allows to get list of github users.{' '}
+          <br />
+          Each row contains login, profile link (html_url) and avatar preview(100x100). <br />
+          <br />
+          Clicking on row should result in opening new route with bigger avatar version and
+          additional <br />
+          user info from "https://api.github.com/users/:username", such as name, followers,
+          following, created_at, company, email, location, blog, bio.
+          <br />
+          <br />
+          We expect application will allow to browse at least first 100 users.
+          <br />
+          <br />
+          Using React, ES2015 and Redux is required.
+        </code>
+      </div>
+    </Fragment>
   );
 };
 
